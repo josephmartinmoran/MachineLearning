af013bd6069a6d2cf2175537010804f42f2875fe
josephmartinmoran <josephmmoran11@gmail.com>
2023-02-13 15:39:18 -0500

    Full Send

---
+++ Classification.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn import preprocessing, neighbors
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+df = pd.read_csv('breast_cancer_wisconsin.data')
+df.replace('?', -99999, inplace=True)
+# Gets rid of missing data with outlier footholder
+df.drop(['id'], 1, inplace=True)
+
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+
+accuracies = []
+
+for i in range(25):
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+    # tests on 20% of the data
+
+    clf = neighbors.KNeighborsClassifier(n_jobs=-1)
+    clf.fit(X_train, y_train)
+
+    accuracy = clf.score(X_test, y_test)
+    #print(accuracy)
+
+    #example_measures = np.array([[4,2,1,1,1,2,3,2,1], [4,2,1,2,2,2,3,2,1]])
+    #example_measures = example_measures.reshape(len(example_measures),-1)
+    # Predicts on any number of examples
+
+    #prediction = clf.predict(example_measures)
+    #print(prediction)
+
+    accuracies.append(accuracy)
+
+print(sum(accuracies)/len(accuracies))
--- regression.py
+++ regression.py
@@ -16,13 +16,14 @@ df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
 
+#           Price         X           X             X
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 # Treats nans as outliers
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 # rounds up to an integer
 
 df['label'] = df[forecast_col].shift(-forecast_out)
@@ -30,8 +31,8 @@ df['label'] = df[forecast_col].shift(-forecast_out)
 
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
-X = X[:-forecast_out]
 X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
